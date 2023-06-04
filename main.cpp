8589da03c4ef3b6b21d4f29d758b1cc5d9a16441
Oleksandr-xp28 <fedorut20@gmail.com>
2023-06-04 10:11:39 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// multidimensional arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
+// In the future, to open this project again, go to File > Open > Project and select the .sln file
