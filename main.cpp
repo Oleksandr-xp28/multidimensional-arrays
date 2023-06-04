7fa312b7bd996060328b9968b2d59fb9982b4380
Oleksandr-xp28 <fedorut20@gmail.com>
2023-06-04 10:24:11 +0300

    task done

--- main.cpp
+++ main.cpp
@@ -4,9 +4,43 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    return 0;
-}
+int main() {
+	
+	const int rows = 3;
+	const int cols = 4;
 
+	int arr[rows][cols] = {
+	{ 1, 2, 3, 4 },
+	{ 5, 6, 7, 8 },
+	{ 9, 10, 11, 12 }
+	};
+
+	int sum = 0;
+	int count = 0;
+	int min = arr[0][0];
+	int max = arr[0][0];
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			sum += arr[i][j];
+			count++;
+
+			if (arr[i][j] < min) {
+				min = arr[i][j];
+			}
+
+			if (arr[i][j] > max) {
+				max = arr[i][j];
+			}
+		}
+	}
+
+	double mean = double(sum) / count;
+
+	cout << "Sum: " << sum << endl;
+	cout << "Mean: " << mean << endl;
+	cout << "Minimum: " << min << endl;
+	cout << "Maximum: " << max << endl;
+	return 0;
+}
 // In the future, to open this project again, go to File > Open > Project and select the .sln file
